0e555344acc56c99ea77b5ac7a08006d3a1f0ef9
Yasin <86363885+yOzcc@users.noreply.github.com>
2024-05-08 13:05:54 +0300

    rest-api

---
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package net.javaguides.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //HTTP GET Request
+    //http://localhost:8080/hello-world
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World! ";
+    }
+}
